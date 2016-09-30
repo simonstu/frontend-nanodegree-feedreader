1c7def5c1a82247b098f6c1de444960a8cf34064
Simon Stusak <simon.stusak@ifi.lmu.de>
2016-09-30 12:06:11 +0200

    add tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,42 +31,98 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('have a not empty URL', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have a not empty name', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('changes visibility when the icon is clicked', function() {
+            $('.menu-icon-link').trigger( "click" );
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').trigger( "click" );
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            setTimeout(function() {
+                done();
+            }, 1500);
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        it('are loaded', function(done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var initialFeedEntryHref;
+
+        beforeEach(function(done) {
+            setTimeout(function() {
+                // initial entries of the first feed should be loaded -> save link of first entry
+                initialFeedEntryHref = $('.feed > .entry-link').first().attr("href");
+                // click last element in feed list
+                $('.feed-list li a').last().trigger( "click" );
+                // start another timer to wait for new entries of the feed to load
+                setTimeout(function() {
+                    done();
+                }, 3000);
+            }, 1500);
+        });
+
+        it('loads new feed entries', function(done) {
+            // new entries of the feed should be loaded -> save href of the first element
+            var newLoadedFeedEntryHref = $('.feed > .entry-link').first().attr("href");
+            // as a new feed was loaded, the first entry should have a different href than the previous one
+            expect(initialFeedEntryHref).not.toBe(newLoadedFeedEntryHref);
+            done();
+        });
+    });
 }());
